0cbaf731695b997c6bc4897f45abed96e0af8633
Matthew Irish <matthew.irish@gmail.com>
2011-08-13 16:28:33 -0500

    changed 'media' folder to 'static' and added a bunch of css and the init for the page to resize the map and kick off the slider

--- pythonkc_site/settings.py
+++ pythonkc_site/settings.py
@@ -60,7 +60,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static'),
+#STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static'),
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -73,6 +73,7 @@ ADMIN_MEDIA_PREFIX = '/static/admin/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
--- pythonkc_site/urls.py
+++ pythonkc_site/urls.py
@@ -19,3 +19,5 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
 )
+
+
