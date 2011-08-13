9e280709c01eb1230274697b2ea508f9ba5e48fd
Steven Cummings <estebistec@gmail.com>
2011-08-13 11:06:17 -0500

    Fixes to wsgi.py (maybe I should actually have our project name in there and not the example... hur dur!)

--- pythonkc_site/deploy/wsgi.py
+++ pythonkc_site/deploy/wsgi.py
@@ -1,5 +1,8 @@
 import os, sys
 
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.abspath(os.path.dirname(__file__)), os.pardir, os.pardir)))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.abspath(os.path.dirname(__file__)), os.pardir)))
+
 from django.core.handlers.wsgi import WSGIHandler
-os.environ["DJANGO_SETTINGS_MODULE"] = "myproject.settings"
+os.environ["DJANGO_SETTINGS_MODULE"] = "pythonkc_site.settings"
 application = WSGIHandler()
