5b0eb4f09f241fe006c3935efc474401f6d71cde
Jon Smajda <jon@smajda.com>
2013-09-26 06:40:11 -0500

    Use environ.get for optional env vars.

--- pythonkc_site/settings.py
+++ pythonkc_site/settings.py
@@ -163,8 +163,8 @@ CONTACT_EMAIL_TO = ['pythonkc@gmail.com']
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ["EMAIL_HOST_USER"]
-EMAIL_HOST_PASSWORD = os.environ["EMAIL_HOST_PASSWORD"]
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 MEETUP_API_KEY = os.environ["MEETUP_API_KEY"]
 
 try:
@@ -174,7 +174,7 @@ except ImportError:
 
 
 try:
-    # NOTE The GONDOR_REDIS_* settings are placed in local_settings by the 
+    # NOTE The GONDOR_REDIS_* settings are placed in local_settings by the
     # Gondor runtime.
     CACHES = {
         'default': {
