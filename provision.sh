239115be3a77b50a190109bab9e5c5125b0f5a93
Jon Smajda <jon@smajda.com>
2015-06-10 14:34:54 -0500

    Initial ansible setup

--- provision.sh
+++ provision.sh
@@ -3,8 +3,13 @@
 
 export DEBIAN_FRONTEND=noninteractive
 
-aptitude update
-aptitude dist-upgrade -y
-aptitude install -y python3 python3-dev python3-pip ansible
 ln -sf /vagrant /home/vagrant/
-# TODO: Run Ansible playbooks
+
+if [[ -z "$(which ansible)" ]]; then
+    echo "Installing ansible"
+    aptitude update
+    aptitude install -y python3 python3-dev python3-pip ansible
+fi
+
+cd /home/vagrant/vagrant/ansible
+ansible-playbook vagrant.yml
